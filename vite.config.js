06685a364409bd76b4ec58ef63c9b31d3c74f226
adrianzafier2-tech <adrianzafier2@gmail.com>
2026-09-04 05:29:14 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Qynex-Green-Circuit/', // Make sure the leading and trailing slashes are present
+  base: '/Qynex-Green-Circuit/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
